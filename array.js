92c2c44947393015409352e63ba987e4dfd741a3
Nick Johnson <nicktjohnson93@gmail.com>
2018-01-18 04:37:53 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,17 +1,21 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+ return(element, ...array)
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  array.shift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
-
+return(...array, element)
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+array.push(element)
+return array
 }
 
 function accessElementInArray(array, index) {
@@ -19,16 +23,25 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+array.shift(element)
+return(array)
 }
 
 function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+array.pop(element)
+return(array)
 }
 
 function removeElementFromEndOfArray(array) {
+return array.slice (0, array.length -1)
+}
 
+//challenge: remove and element from the middle of an array/at any given point, and return a copy of the original array, minus that element
+
+function nonDestructivelyRemoveAtIndex(array, index) {
+  //hint: involves slice and spread
 }
